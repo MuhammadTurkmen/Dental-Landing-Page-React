15ca8df53e2bbcbd9a760314b7cda096d3927fb3
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-04-14 14:32:50 +0430

    adding div

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,7 +1,36 @@
 import React from "react";
+import styled from "styled-components";
 
 function Services() {
-  return <div>Services</div>;
+  return (
+    <Wrapper>
+      <p className="title">Services</p>
+      <div className="text-container">
+        <div className="clinic-services">
+          <h1>Clinic Services</h1>
+          <p className="desc">
+            we use the best quality materials on the market in order to provide
+            the best products to our patients
+          </p>
+        </div>
+        <a href="#">More Services</a>
+      </div>
+      <div className="card-container"></div>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  background: var(--lighter-green, #e5efe8);
+
+  .title {
+    background-color: #f2e9b7;
+    padding: 5px 10px;
+    font-size: 10px;
+    border-radius: 5px;
+    display: inline-block;
+    color: var(--dark-green, #395a44);
+  }
+`;
 
 export default Services;
