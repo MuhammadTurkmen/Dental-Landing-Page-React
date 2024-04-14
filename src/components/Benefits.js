fb5adf5a2ae8b02da3862a72ba519735c6bfe1c2
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-04-14 15:39:07 +0430

    adding img

--- src/components/Benefits.js
+++ src/components/Benefits.js
@@ -1,13 +1,41 @@
 import React from "react";
 import styled from "styled-components";
+import Ann from "../images/Ann-1.jpg";
+import tooth from "../images/svg/icons8-tooth-50 1.png";
 
 function Benefits() {
-  return <Wrapper>Benefits </Wrapper>;
+  return (
+    <Wrapper>
+      <p className="Benefits">Benefits</p>
+      <div className="container">
+        <div className="img-container">
+          <img className="ann" src={Ann} alt="doctor" />
+          <div className="teeth-container"></div>
+        </div>
+        <div className="text-container"></div>
+      </div>
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.div`
   background-color: #395a44;
   color: #e5efe8;
+  text-align: center;
+  padding: 6rem 1.5rem;
+
+  .ann,
+  .teeth-container {
+    display: none;
+  }
+  .Benefits {
+    background-color: #f2e9b7;
+    padding: 5px 10px;
+    font-size: 10px;
+    border-radius: 5px;
+    display: inline-block;
+    color: var(--dark-green, #395a44);
+  }
 `;
 
 export default Benefits;
