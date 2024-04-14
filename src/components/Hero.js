6847d8b1f170bc603e6429161e59589f229b99da
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-04-14 13:24:03 +0430

    adding margin

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -15,12 +15,17 @@ function Hero() {
             We will give you the best smile you deserve with complete and modern
             facilities services only at our dental clinic.
           </p>
-          <button className="dark-btn">OK</button>
-          <button className="light-btn">Not Now</button>
+          <button className="dark btn">OK</button>
+          <button className="light btn">Not Now</button>
           <div className="smile-container">
-            {smile_pics.map(({ smile, id }) => {
-              return <img key={id} src={smile} alt="smile" />;
-            })}
+            <div>
+              {smile_pics.map(({ smile, id }) => {
+                return (
+                  <img className="smile" key={id} src={smile} alt="smile" />
+                );
+              })}
+            </div>
+            <p>smile</p>
           </div>
         </div>
         <div className="img-container">
@@ -37,10 +42,36 @@ const Wrapper = styled.div`
 
   .title {
     font-size: 30px;
+    color: var(--dark-green, #395a44);
+  }
+  .desc {
+    margin-bottom: 3rem;
+    font-size: 14px;
+    color: #395a44;
   }
 
   .main-img {
     display: none;
+  }
+  .btn {
+    padding: 0.8rem 3rem;
+    border-radius: 8px;
+    border: 1px solid #395a44;
+    transition: all 0.3s ease-in-out;
+    margin-bottom: 3rem;
+  }
+  .btn:hover {
+    transform: scale(1.1);
+  }
+
+  .dark {
+    margin-right: 1rem;
+    background-color: #395a44;
+    color: #fff;
+    padding: 0.8rem 3.5rem;
+  }
+  .smile {
+    margin-left: -1.5rem;
   }
 `;
 
