d215a5a07e01dadb2b65d259e195ad9c18c69289
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-04-14 13:02:36 +0430

    adding styled

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,19 @@
 import React from "react";
+import styled from "styled-components";
 
 function Hero() {
-  return <div>Hero </div>;
+  return (
+    <Wrapper>
+      <div className="container">
+        <div className="text-container"></div>
+        <div className="img-container"></div>
+      </div>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  background-color: black;
+`;
 
 export default Hero;
