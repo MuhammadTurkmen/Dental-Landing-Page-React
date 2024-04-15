fb2fac2fb951e0a848a36474d49c27a0c4b6c39e
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-04-15 22:08:38 +0430

    adding title

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -2,11 +2,23 @@ import React from "react";
 import styled from "styled-components";
 
 function Testimonial() {
-  return <Wrapper>Testimonial</Wrapper>;
+  return (
+    <Wrapper>
+      <p className="title">Specialist</p>
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.div`
   background: var(--Light-green, #cfe3cc);
+  .title {
+    background-color: #f2e9b7;
+    padding: 5px 10px;
+    font-size: 10px;
+    border-radius: 5px;
+    display: inline-block;
+    color: var(--dark-green, #395a44);
+  }
 `;
 
 export default Testimonial;
