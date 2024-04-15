348c7b06feef83c935741dc054aa13eec30cce1c
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-04-15 08:16:07 +0430

    adding h1 and p

--- src/components/ServicesPageComponents/Services.js
+++ src/components/ServicesPageComponents/Services.js
@@ -4,7 +4,13 @@ import styled from "styled-components";
 function Services() {
   return (
     <Wrapper>
-      <header></header>
+      <header>
+        <h1>Services</h1>
+        <p>
+          We use the best quality materials on the mmarket <br /> in oerder to
+          provide the best products to our patients.
+        </p>
+      </header>
       <main></main>
     </Wrapper>
   );
