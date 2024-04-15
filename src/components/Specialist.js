bf94b00f9b2d62da0a24240710ffdd3898606c55
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-04-15 16:18:49 +0430

    adding background

--- src/components/Specialist.js
+++ src/components/Specialist.js
@@ -1,7 +1,12 @@
 import React from "react";
+import styled from "styled-components";
 
 function Specialist() {
   return <div>Specialist</div>;
 }
+
+const Wrapper = styled.div`
+  background-color: var(--lighter-green, #e5efe8);
+`;
 
 export default Specialist;
