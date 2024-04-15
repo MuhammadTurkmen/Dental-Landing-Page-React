99fc9c91b5db555f1cf51653ad147c50646b8136
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-04-15 19:49:33 +0430

    addingp

--- src/components/Specialist.js
+++ src/components/Specialist.js
@@ -2,11 +2,21 @@ import React from "react";
 import styled from "styled-components";
 
 function Specialist() {
-  return <div>Specialist</div>;
+  return (
+    <Wrapper>
+      <div className="text-container">
+        <h1>Meet Our Professinal Doctors</h1>
+        <p>
+          We have the professional doctors to handle any dental case, and make
+          you smile agian
+        </p>
+      </div>
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.div`
-  background-color: var(--lighter-green, #e5efe8);
+  background: var(--lighter-green, #e5efe8);
 `;
 
 export default Specialist;
