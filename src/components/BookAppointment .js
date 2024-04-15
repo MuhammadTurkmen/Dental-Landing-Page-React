993c7a904849489d607f23ad67c61de83ed442ab
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-04-15 08:30:45 +0430

    adding p

--- src/components/BookAppointment .js
+++ src/components/BookAppointment .js
@@ -2,11 +2,23 @@ import React from "react";
 import styled from "styled-components";
 
 function BookAppointment() {
-  return <Wrapper>Book Appointment </Wrapper>;
+  return (
+    <Wrapper>
+      <p className="title">Specialist</p>
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.div`
   background: var(--dark-green, #395a44);
+  .title {
+    background-color: #f2e9b7;
+    padding: 5px 10px;
+    font-size: 10px;
+    border-radius: 5px;
+    display: inline-block;
+    color: var(--dark-green, #395a44);
+  }
 `;
 
 export default BookAppointment;
