9731c933651639e3b012ea1d1d5acaeffb3b8ef7
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-04-17 19:18:57 +0430

    init

--- src/components/ServicesPageComponents/Services.js
+++ src/components/ServicesPageComponents/Services.js
@@ -21,7 +21,7 @@ function Services() {
                 <img src={tooth} alt="tooth" />
                 <h1>{title}</h1>
                 <p>{desc}</p>
-                <Link to="/" className="btn">
+                <Link to="/#book-appointment" className="btn">
                   Learn more
                 </Link>
               </div>
